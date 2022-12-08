5068feddd44be3961940eef753763b5c126ab2e2
kelvin <kelvinmwenda9@gmail.com>
2022-12-08 15:03:26 +0300

    recursion algorithms

---
+++ recursion.py
@@ -0,0 +1,11 @@
+# recursion refers to a method or function that will call itself. it is used to resolved problems by breaking 
+# the problem down into sub-problems.
+# ############ recursive algorithms
+# divide and conquer
+# ### consists two parts, 1, breaking problem to sub-problems, 2, solving the final solution
+# dynamic programming
+# @@@ mainly used for optimization problems. similar to the divide and conquer algorithm in that it splits 
+# the problems into sub-problems
+# greedy alrgorithm
+# @@@ finds the best solution in each and every step instead of approaching
+# optimization in a global way
