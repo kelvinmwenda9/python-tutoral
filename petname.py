bd7495d7a173239879474eb7b618557bed0b89c4
kelvin <kelvinmwenda9@gmail.com>
2022-12-08 11:47:38 +0300

    New folder and changes

--- petname.py
+++ petname.py
@@ -1,1 +1,12 @@
-# with open('petsname.txt', 'r') as file:
+import random
+
+filed = input('enter file name: ')
+
+with open(filed, 'r') as file:
+    data = file.read()
+    print(data)
+    # convert txt into list values
+    data_f = data.split('\n')
+    print(data_f)
+
+    print(random.choice(data_f))
